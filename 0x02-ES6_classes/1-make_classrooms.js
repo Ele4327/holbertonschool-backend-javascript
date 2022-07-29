efe508c5e1f917c4d6d6dbef94d28884d53c7977
L <sebasgarzons88@gmail.com>
2022-07-28 20:42:57 -0500

    Fixed import from, and return

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,8 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
-    let room0 = new ClassRoom(19);
-    let room1 = new ClassRoom(20);
-    let room2 = new ClassRoom(34);
-    const totalroom = [room0, room1, room2];
-    return [totalroom];
+export default function initializeRooms() {
+  const room0 = new ClassRoom(19);
+  const room1 = new ClassRoom(20);
+  const room2 = new ClassRoom(34);
+  return [room0, room1, room2];
 }

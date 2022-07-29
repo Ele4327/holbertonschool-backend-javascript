e00a823c3934564e2696e92dc7bb2cd01eb0f80c
L <sebasgarzons88@gmail.com>
2022-07-28 21:02:41 -0500

    Static Methods

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,24 @@
+export default class Currency {
+    constructor(code, name) {
+      this._code = code;
+      this._name = name;
+    }
+
+    get code() {
+      return this._code;
+    }
+    set code(valcode) {
+      this._code = valcode;
+    }
+
+    get name() {
+      return this._name;
+    }
+    set name(valname) {
+      this._name = valname;
+    }
+
+    displayFullCurrency() {
+      return `${this.name}, ${this.code}`;
+    }
+  }

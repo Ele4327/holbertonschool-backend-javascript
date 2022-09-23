2d9180b53c63438ee4d7c3ab9194389badd40401
L <sebasgarzons88@gmail.com>
2022-09-22 22:12:37 -0400

    Third task

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,15 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./3-payment')
+const Utils = require('./utils')
+const sinon = require('sinon')
+const chai = require("chai");
+const expect = chai.expect;
 
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
-
-describe('sendPaymentRequestToApi function', () => {
-  const utilSpy = sinon.spy(Utils, 'calculateNumber');
-
-  it('validate the usage of the Utils function', () => {
-    sendPaymentRequestToApi(100, 20);
-    expect(utilSpy.calledOnce).to.be.true;
-    expect(utilSpy.calledWith('SUM', 100, 20)).to.be.true;
-    utilSpy.restore()
-  });
-});
+describe('Sure the math used', function () {
+	it('Validate the usage of the Utils function', function () {
+		const callback = sinon.spy(Utils, 'calculateNumber')
+		sendPaymentRequestToApi(100, 20);
+		expect(callback.calledOnce).to.be.true;
+		expect(callback.calledWith('SUM', 100, 20)).to.be.true;
+		callback.restore()
+	})
+})

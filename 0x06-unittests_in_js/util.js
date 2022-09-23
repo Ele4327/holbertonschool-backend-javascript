193c51e06002e41185b444e3ba91970ab1937122
L <sebasgarzons88@gmail.com>
2022-09-22 20:43:51 -0400

    Third task

---
+++ 0x06-unittests_in_js/util.js
@@ -0,0 +1,29 @@
+const Utils = {
+    calculateNumber(type, a, b) {
+        const aRound = Math.round(a);
+        const bRound = Math.round(b);
+    
+        if (type === 'SUM') {
+          let sumValue = aRound + bRound
+          return sumValue;
+        }
+    
+        if (type === 'SUBTRACT') {
+          let subValue = aRound - bRound;
+          return subValue;
+        }
+    
+        if (type === 'DIVIDE') {
+          let divValue = aRound / bRound;
+    
+          if (bRound === 0) {
+            return 'Error';
+          }
+    
+          return divValue;
+        }
+    },
+  };
+    
+  module.exports = Utils;
+  
